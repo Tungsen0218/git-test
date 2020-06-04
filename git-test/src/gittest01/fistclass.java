0319b6c99fd11fabd017302f15483aad7f3a95a3
Tungsen0218 <hds520100@qq.com>
2020-06-05 00:19:38 +0800

    iiiiiloveyuni

---
+++ git-test/src/gittest01/fistclass.java
@@ -0,0 +1,13 @@
+package gittest01;
+
+public class fistclass {
+    public static void main(String[] args) {
+        System.out.println("Hello！Git");
+        System.out.println("Hello！Git");
+        System.out.println("Hello！Git");
+        System.out.println("Hello！Git");
+        System.out.println("Hello！Git");
+        System.out.println("Hello！Git");
+
+    }
+}
